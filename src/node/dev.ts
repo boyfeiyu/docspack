398352609ec278e9efe9bd7f4ee4cd5c6ff5bd3c
feiyuer <296862383@qq.com>
2023-07-23 19:42:02 +0800

    feat: 实现react组件的渲染

--- src/node/dev.ts
+++ src/node/dev.ts
@@ -1,10 +1,11 @@
 // https://cn.vitejs.dev/guide/api-javascript.html#createserver
 
 import { createServer } from 'vite'
-
+import { pluginIndexHtml } from './plugin-docspack/indexHtml'
 export async function createDevServer(root = process.cwd()) {
   console.log('root is ', root)
   return createServer({
     root,
+    plugins: [pluginIndexHtml()]
   })
 }
