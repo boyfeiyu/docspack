e929ff99ec2fe99ba4f6fcccf9b072d9a361681b
feiyuer <296862383@qq.com>
2023-07-30 06:43:42 +0800

    fix: vitest排除e2e测试，dev服务器启动暂时使用项目根目录

--- src/node/dev.ts
+++ src/node/dev.ts
@@ -5,12 +5,13 @@ import { pluginIndexHtml } from './plugin-docspack/indexHtml';
 import pluginReact from '@vitejs/plugin-react';
 import { resolveConfig } from './config';
 import { pluginConfig } from './plugin-docspack/config';
+import { PACKAGE_ROOT } from './constants';
 
 export async function createDevServer(root = process.cwd()) {
   const config = await resolveConfig(root, 'serve', 'development');
   console.log(config);
   return createServer({
-    root,
+    root: PACKAGE_ROOT,
     plugins: [pluginIndexHtml(config), pluginReact(), pluginConfig(config)]
   });
 }
