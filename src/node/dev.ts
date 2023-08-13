0e8109bfe323f741c5ee4b9af4200ff8ae395f01
feiyuer <296862383@qq.com>
2023-08-13 15:10:06 +0800

    fix(dev server): 将template.html中js文件改为自动插入，使用绝对路径，修复找不到文件的问题

--- src/node/dev.ts
+++ src/node/dev.ts
@@ -10,14 +10,22 @@ import { pluginRoutes } from './plugin-routes';
 import { PACKAGE_ROOT } from './constants';
 
 export async function createDevServer(root = process.cwd()) {
+  // FIXME dev访问路由时偶尔会直接返回静态资源
   const config = await resolveConfig(root, 'serve', 'development');
   return createServer({
     root: PACKAGE_ROOT,
     plugins: [
       pluginIndexHtml(),
-      pluginReact(),
+      pluginReact({
+        jsxRuntime: 'automatic'
+      }),
       pluginConfig(config),
       pluginRoutes({ root: config.root })
-    ]
+    ],
+    server: {
+      fs: {
+        allow: [PACKAGE_ROOT]
+      }
+    }
   });
 }
--- src/node/plugin-docspack/indexHtml.ts
+++ src/node/plugin-docspack/indexHtml.ts
@@ -1,11 +1,27 @@
 import { readFile } from 'fs/promises';
 import { Plugin } from 'vite';
-import { DEFAULT_HTML_PATH } from '../constants';
+import { CLIENT_ENTRY_PATH, DEFAULT_HTML_PATH } from '../constants';
 
 export function pluginIndexHtml(): Plugin {
   return {
     name: 'docspack:index-html',
     apply: 'serve',
+    // 插入入口 script 标签
+    transformIndexHtml(html) {
+      return {
+        html,
+        tags: [
+          {
+            tag: 'script',
+            attrs: {
+              type: 'module',
+              src: `/@fs/${CLIENT_ENTRY_PATH}`
+            },
+            injectTo: 'body'
+          }
+        ]
+      };
+    },
     configureServer(server) {
       return () => {
         server.middlewares.use(async (req, res, next) => {
